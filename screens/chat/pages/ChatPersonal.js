a88d5e2f64b368b578616980de8dd104cf208d9e
QuangHa0110 <quanghadang08@gmail.com>
2022-12-12 19:20:41 +0700

    [CODE] init chat personal page

---
+++ screens/chat/pages/ChatPersonal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ChatPersonal = () => {
+  return (
+    <View>
+      <Text>Chat Personal</Text>
+    </View>
+  );
+};
+
+export default ChatPersonal;
