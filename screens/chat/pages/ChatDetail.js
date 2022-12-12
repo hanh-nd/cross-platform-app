b877867fb59b1c3b88ec1a6959d4dde128345269
QuangHa0110 <quanghadang08@gmail.com>
2022-12-12 19:20:29 +0700

    [CODE] update header chat detail

--- screens/chat/pages/ChatDetail.js
+++ screens/chat/pages/ChatDetail.js
@@ -1,7 +1,9 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Avatar } from "@rneui/themed";
-import React, { useEffect } from "react";
+import { Avatar, BottomSheet, Icon, ListItem } from "@rneui/themed";
+import React, { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
+import { colors } from "../../../constants";
+import { PageName } from "../../../navigation/constants";
 import ConversationHeader from "../components/ConversationHeader";
 
 function ChatDetail(props) {
@@ -9,11 +11,25 @@ function ChatDetail(props) {
   const navigation = useNavigation();
   const route = useRoute();
   const item = route.params.item;
+
   useEffect(() => {
     navigation.setOptions({
       headerTitle: () => <ConversationHeader item={item} />,
+      headerRight: () => (
+        <Icon
+          type="font-awesome"
+          name="info-circle"
+          color={colors.grayBlue}
+          onPress={() => {
+            navigation.navigate({
+              name: PageName.CHAT_PERSONAL,
+            });
+          }}
+        />
+      ),
     });
   }, []);
+
   return (
     <View>
       <Text>Chate Detail</Text>
