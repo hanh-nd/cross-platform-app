86f6869bb8d5697b1065796e2408a336fa20f2ac
QuangHa0110 <quanghadang08@gmail.com>
2022-12-12 19:20:11 +0700

    [CODE] update router

--- navigation/routers.js
+++ navigation/routers.js
@@ -1,109 +1,109 @@
-import { Icon } from '@rneui/themed';
+import { Icon } from "@rneui/themed";
 import {
-    Chat,
-    Home,
-    Loading,
-    Logout,
-    Login,
-    Register,
-    Profile,
-    ListFriends,
-    EditProfile,
-    EditUser,
-    CreatePostPage,
-} from '../screens';
-import TabNavigator from './TabNavigator';
-import { PageName } from './constants';
-import ChatDetail from '../screens/chat/pages/ChatDetail';
+  Chat,
+  Home,
+  Loading,
+  Logout,
+  Login,
+  Register,
+  Profile,
+  ListFriends,
+  EditProfile,
+  EditUser,
+  CreatePostPage,
+} from "../screens";
+import TabNavigator from "./TabNavigator";
+import { PageName } from "./constants";
+import ChatDetail from "../screens/chat/pages/ChatDetail";
+import ChatPersonal from "../screens/chat/pages/ChatPersonal";
 
 export const routes = [
-    {
-        name: PageName.LOADING,
-        component: Loading,
-        headerShown: false,
-    },
-    {
-        name: PageName.TAB_NAVIGATOR,
-        component: TabNavigator,
-        headerShown: false,
-    },
-    {
-        name: PageName.LOGIN,
-        component: Login,
-        headerShown: false,
-    },
-    {
-        name: PageName.LOGOUT,
-        component: Logout,
-        headerShown: false,
-    },
-    {
-        name: PageName.REGISTER,
-        component: Register,
-        headerShown: false,
-    },
-    {
-        name: PageName.HOME,
-        component: Home,
-        headerShown: false,
-    },
-    {
-        name: PageName.CHAT,
-        component: Chat,
-        headerShown: false,
-    },
-    {
-        name: PageName.PROFILE,
-        component: Profile,
-        headerShown: false,
-    },
-    {
-        name: PageName.LIST_FRIENDS,
-        component: ListFriends,
-        headerShown: true,
-    },
-    {
-        name: PageName.EDIT_PROFILE,
-        component: EditProfile,
-        headerShown: true,
-    },
-    {
-        name: PageName.EDIT_USER,
-        component: EditUser,
-        headerShown: true,
-    },
-    {
-        name: PageName.CREATE_POST_PAGE,
-        component: CreatePostPage,
-        headerShown: true,
-    },
-    {
-        name: PageName.CHAT_DETAIL,
-        component: ChatDetail,
-        headerShown: true,
-    }
+  {
+    name: PageName.LOADING,
+    component: Loading,
+    headerShown: false,
+  },
+  {
+    name: PageName.TAB_NAVIGATOR,
+    component: TabNavigator,
+    headerShown: false,
+  },
+  {
+    name: PageName.LOGIN,
+    component: Login,
+    headerShown: false,
+  },
+  {
+    name: PageName.LOGOUT,
+    component: Logout,
+    headerShown: false,
+  },
+  {
+    name: PageName.REGISTER,
+    component: Register,
+    headerShown: false,
+  },
+  {
+    name: PageName.HOME,
+    component: Home,
+    headerShown: false,
+  },
+  {
+    name: PageName.CHAT,
+    component: Chat,
+    headerShown: false,
+  },
+  {
+    name: PageName.PROFILE,
+    component: Profile,
+    headerShown: false,
+  },
+  {
+    name: PageName.LIST_FRIENDS,
+    component: ListFriends,
+    headerShown: true,
+  },
+  {
+    name: PageName.EDIT_PROFILE,
+    component: EditProfile,
+    headerShown: true,
+  },
+  {
+    name: PageName.EDIT_USER,
+    component: EditUser,
+    headerShown: true,
+  },
+  {
+    name: PageName.CREATE_POST_PAGE,
+    component: CreatePostPage,
+    headerShown: true,
+  },
+  {
+    name: PageName.CHAT_DETAIL,
+    component: ChatDetail,
+    headerShown: true,
+  },
+  {
+    name: PageName.CHAT_PERSONAL,
+    component: ChatPersonal,
+    headerShown: true,
+  },
 ];
 
 export const tabNavigatorRoutes = [
-    {
-        name: PageName.HOME,
-        component: Home,
-        icon: ({ color, size }) => (
-            <Icon name="home" color={color} size={size} />
-        ),
-    },
-    {
-        name: PageName.CHAT,
-        component: Chat,
-        icon: ({ color, size }) => (
-            <Icon name="chat" color={color} size={size} />
-        ),
-    },
-    {
-        name: PageName.PROFILE,
-        component: Profile,
-        icon: ({ color, size }) => (
-            <Icon name="person" color={color} size={size} />
-        ),
-    },
+  {
+    name: PageName.HOME,
+    component: Home,
+    icon: ({ color, size }) => <Icon name="home" color={color} size={size} />,
+  },
+  {
+    name: PageName.CHAT,
+    component: Chat,
+    icon: ({ color, size }) => <Icon name="chat" color={color} size={size} />,
+  },
+  {
+    name: PageName.PROFILE,
+    component: Profile,
+    icon: ({ color, size }) => <Icon name="person" color={color} size={size} />,
+  },
 ];
