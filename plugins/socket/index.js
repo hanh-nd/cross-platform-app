2d80df283d2864a015820f6e2b0f3e01daef19b2
QuangHa0110 <quanghadang08@gmail.com>
2022-12-25 17:34:20 +0700

    [CODE] add token to sokect

--- plugins/socket/index.js
+++ plugins/socket/index.js
@@ -1,19 +1,35 @@
 import { env } from '@/constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { io } from 'socket.io-client';
+import { getAccessToken } from '../axios/axios';
 
-class SocketProvider {
-    static _socket;
+// class SocketProvider {
+//     static _socket;
 
-    static initialize = () => {
-        SocketProvider._socket = io.connect(env.SOCKET_URL);
-    };
+//     static initialize = () => {
+//         SocketProvider._socket = io.connect(env.SOCKET_URL);
+//     };
 
-    static getSocket = () => {
-        if (!SocketProvider._socket) {
-            SocketProvider.initialize();
-        }
-        return SocketProvider._socket;
-    };
+//     static getSocket = () => {
+//         if (!SocketProvider._socket) {
+//             SocketProvider.initialize();
+//         }
+//         return SocketProvider._socket;
+//     };
+// }
+// export default SocketProvider.getSocket();
+
+
+
+const Socket = async () => {
+
+    const token = await getAccessToken();
+    // console.log("🚀 ~ file: index.js:27 ~ getSocket ~ token", token)
+    const socket = io.connect(env.SOCKET_URL, {
+        extraHeaders: {
+            token
+        },
+    });
+    return socket;
 }
-
-export default SocketProvider.getSocket();
+export default Socket;
